90751700a32e3b61e2dc87c88f3630f541786914
Pawel Fajfer <fajfer.pawel@gmail.com>
2024-04-29 20:03:18 +0200

    Added first world assets

---
+++ player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+var speed = 400
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
